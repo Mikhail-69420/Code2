959f331d2c576147f01ef3b735632eff9a67711f
Mikhail-69420 <leederko@gmail.com>
2024-10-21 11:35:10 +0200

    fix

---
+++ Lintertest/LinterTest.ts
@@ -0,0 +1,21 @@
+namespace LinterTest {
+
+  interface Info {
+    text: string, key: KEY
+  }
+
+  enum KEY {
+    POS = 1, NEG = -1
+  }
+  
+  const info: Info = { text: "G`udetmvhsgBncd1 ", key: KEY.POS };
+  console.log(deCrypt(info.text, info.key));
+
+  function deCrypt(_text: string, _key: number): string {
+    let result: string = "";
+    for (let i: number = 0; i < _text.length; i++) {
+      result += String.fromCharCode(_text.charCodeAt(i) + _key)
+    }
+    return result;
+  }
+}
