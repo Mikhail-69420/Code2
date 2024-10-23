057d6f6f253f84f4635f1aa3534375a8518bfd44
Mikhail-69420 <leederko@gmail.com>
2024-10-23 16:19:27 +0200

    oldmcdonalds

---
+++ oldmcdonaldsfarm/oldmcdonaldsfarm.ts
@@ -0,0 +1,18 @@
+class Animal {
+    name: string;
+    species: string;
+    foodType: string;
+    foodAmount: number;
+    sound: string;
+
+    constructor(_name: string, _species: string, _foodtype: string, _foodAmount: number, _sound: string){
+        this.set(_name, _species, _foodtype, _foodAmount, _sound)
+    }
+
+
+
+
+
+
+}
+
