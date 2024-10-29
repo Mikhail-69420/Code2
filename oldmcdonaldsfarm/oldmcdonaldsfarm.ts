4b0e0b367960bb8faf3170e03aa06b1551bca863
Mikhail-69420 <leederko@gmail.com>
2024-10-29 19:06:37 +0100

    change stuff

--- oldmcdonaldsfarm/oldmcdonaldsfarm.ts
+++ oldmcdonaldsfarm/oldmcdonaldsfarm.ts
@@ -4,10 +4,13 @@ class Animal {
     foodType: string;
     foodAmount: number;
     sound: string;
-
-    constructor(_name: string, _species: string, _foodtype: string, _foodAmount: number, _sound: string){
-        this.set(_name, _species, _foodtype, _foodAmount, _sound)
+ 
+    constructor(_name: string, _species: string, _foodtype: string, _foodAmount: number, _sound: string) 
+    {
+        this.set(_name, _species, _foodtype, _foodAmount, _sound);
     }
+
+    
 
 
 
---
+++ randomextra/xta.ts
@@ -0,0 +1,12 @@
+namespace xta {
+let a: number = 2
+let b: number = 2
+
+let c = a + b
+
+function add(_a: number, _b: number): number {
+    return _a + _b
+}
+
+console.log(c)
+}
